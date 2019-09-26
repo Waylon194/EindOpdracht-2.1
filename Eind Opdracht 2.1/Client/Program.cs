dc6e1d4cfd9f7590429f561488d5371eb7989d64
Waylon <waylon194@hotmail.com>
2019-09-26 14:12:53 +0200

    Added project files
    Also renamed some folders

---
+++ Eind Opdracht 2.1/Client/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        private static NetworkStream stream;
+        private static byte[] buffer = new byte[1024];
+        static string totalBuffer = "";
+
+        static void Main (string[] args)
+        {
+            TcpClient client = new TcpClient();
+            client.Connect("localhost", 80);
+
+            stream = client.GetStream();
+            stream.BeginRead (buffer, 0, buffer.Length, new AsyncCallback(OnRead), null);
+
+            Write("login\r\njohan\r\njohan\r\n\r\n");
+
+            while (true)
+            {
+                Console.WriteLine("enter a message");
+                string line = Console.ReadLine();
+                Write($"broadcast\r\n{line}\r\n\r\n");
+            }
+        }
+
+        private static void Write (string v)
+        {
+            stream.Write(System.Text.Encoding.ASCII.GetBytes(v), 0, v.Length);
+            stream.Flush();
+        }
+
+        private static void OnRead (IAsyncResult ar)
+        {
+            Console.WriteLine("got data");
+            int receivedBytes = stream.EndRead(ar);
+            totalBuffer += System.Text.Encoding.ASCII.GetString(buffer, 0, receivedBytes);
+
+            while (totalBuffer.Contains("\r\n\r\n"))
+            {
+                string packet = totalBuffer.Substring(0, totalBuffer.IndexOf("\r\n\r\n"));
+                totalBuffer = totalBuffer.Substring(totalBuffer.IndexOf("\r\n\r\n") + 4);
+                string[] data = Regex.Split(packet, "\r\n");
+                handlePacket(data);
+            }
+            stream.BeginRead(buffer, 0, buffer.Length, new AsyncCallback(OnRead), null);
+        }
+
+        private static void handlePacket(string[] data)
+        {
+            switch (data[0])
+            {
+                case "username":
+                    Console.WriteLine($"Je bent ingelogd: {data[1]}");
+                    break;
+                case "message":
+                    Console.WriteLine($"Bericht van {data[1]}: {data[2]}");
+                    break;
+                default:
+                    Console.WriteLine("Unknown packet");
+                    break;
+            }
+        }
+    }
+}
---
+++ Eind Opdracht 2.1/Server/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServerDemo
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            new Program();
+        }
+
+        TcpListener listener;
+        private List<Client> clients = new List<Client>();
+
+        Program()
+        {
+            listener = new TcpListener (IPAddress.Any, 80);
+            listener.Start();
+            listener.BeginAcceptTcpClient(new AsyncCallback(OnConnect), null);
+            Console.ReadKey();
+        }
+
+        private void OnConnect (IAsyncResult ar)
+        {
+            var newTcpClient = listener.EndAcceptTcpClient(ar);
+            Console.WriteLine("New client connected");
+            clients.Add(new Client(newTcpClient, this));
+
+            listener.BeginAcceptTcpClient(new AsyncCallback(OnConnect), null);
+        }
+    }
+}
